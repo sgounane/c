34a925b0510f5ede68bab7966633b64b2181c9ce
gounane <gounane.said@gmail.com>
2024-05-20 13:24:54 +0100

    list

--- listesChainees/list_pointeur.c
+++ listesChainees/list_pointeur.c
@@ -9,16 +9,17 @@ typedef struct Etudiant
     struct Etudiant *next;
 } Student;
 
-Student readEtudiant()
+Student *readEtudiant()
 {
-    Student e;
+    Student *e;
+    e = (Student *)malloc(sizeof(Student));
     printf("Nom: ");
-    scanf("%s", e.nom);
+    scanf("%s", e->nom);
     printf("Age: ");
-    scanf("%d", &(e.age));
+    scanf("%d", &(e->age));
     printf("Moy: ");
-    scanf("%f", &(e.moy));
-    e.next = NULL;
+    scanf("%f", &(e->moy));
+    e->next = NULL;
     return e;
 }
 
@@ -32,19 +33,15 @@ void insert_End(Student **pdebut, Student *e)
 {
     if (*pdebut == NULL)
     {
-        printf("debut= NULL\n");
         *pdebut = e;
     }
 
     else
     {
         Student *p = *pdebut;
-        printf("avant while %s", p->nom);
         while (p->next != NULL)
         {
-            printf(" While %s", p->nom);
             p = p->next;
-            
         }
 
         p->next = e;
@@ -54,9 +51,10 @@ void insert_End(Student **pdebut, Student *e)
 void print_list(Student *p)
 {
     int i = 0;
+    printf("=========Print List==========\n");
     while (i < 3)
     {
-        printf("%s \t %d \t %f %x\n", p->nom, p->age, p->moy, p->next);
+        printf("%s \t %d \t %f\n", p->nom, p->age, p->moy);
         p = p->next;
         i++;
     }
@@ -66,12 +64,12 @@ int main()
 {
     Student *debut;
     debut = init();
-    Student e = readEtudiant();
-    insert_End(&debut, &e);
+    Student *e = readEtudiant();
+    insert_End(&debut, e);
     e = readEtudiant();
-    insert_End(&debut, &e);
+    insert_End(&debut, e);
     e = readEtudiant();
-    insert_End(&debut, &e);
+    insert_End(&debut, e);
 
     print_list(debut);
     return 0;
